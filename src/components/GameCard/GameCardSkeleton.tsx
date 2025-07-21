12b8e3432a18e90503813b9ddd22d15698ddf400
catherayn-github <bellocatherine022@gmail.com>
2025-07-21 10:35:10 +0800

    Created a Card Container to avoid duplication of styles

--- src/components/GameCard/GameCardSkeleton.tsx
+++ src/components/GameCard/GameCardSkeleton.tsx
@@ -1,13 +1,17 @@
 import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react"
 
 
+
 function GameCardSkeleton() {
   return (
-    <Card width='270px' borderRadius={10} overflow={"hidden"}>
+    <>
+      <Card>
         <Skeleton height="200px" />
         <CardBody></CardBody>
         <SkeletonText marginBottom={4}></SkeletonText>
-    </Card>
+     </Card>
+    </>
+  
   )
 }
 
--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -1,23 +1,31 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../../services/api-clients";
-import { Card, Text, Image, SimpleGrid } from "@chakra-ui/react";
+
+import {SimpleGrid } from "@chakra-ui/react";
 import useGames from "../../hooks/useGames";
 import GameCard from "../GameCard/GameCard";
 import GameCardSkeleton from "../GameCard/GameCardSkeleton";
-
-
+import GameCardContainer from "../GameCard/GameCardContainer";
 
 function GameGrid() {
   const { games, isLoading } = useGames();
-  const skeletonCards = [1,2,3,4,5,6,7,8,9,10];
+  const skeletonCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <div>
-      
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl:5}} padding= "20px" spacing={10}>
-        {isLoading && skeletonCards.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="20px"
+        spacing={10}
+      >
+        {isLoading &&
+          skeletonCards.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </div>
